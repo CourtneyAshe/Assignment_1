b3a2a67f89a42f20d5ba70d7b4232dcb16952b57
CourtneyAshe <courtneyashe94@gmail.com>
2020-08-17 11:55:42 +1200

    Database

--- run.py
+++ run.py
@@ -7,5 +7,43 @@ MENUDB = 'menu.db'
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    db = sqlite3.connect(MENUDB)
 
+    genres = []
+    cur = db.execute('SELECT genre FROM Genres')
+    for row in cur:
+        genres.append(list(row))
+
+    aesthetics = []
+    cur = db.execute('SELECT aesthetic FROM Aesthetics')
+    for row in cur:
+        aesthetics.append(list(row))
+    
+    characters = []
+    cur = db.execute('SELECT character FROM Characters')
+    for row in cur:
+        characters.append(list(row))
+
+    goals = []
+    cur = db.execute('SELECT goal FROM Goals')
+    for row in cur:
+        goals.append(list(row))
+
+    mechanics = []
+    cur = db.execute('SELECT mechanic FROM Mechanics')
+    for row in cur:
+        mechanics.append(list(row))
+
+    adjectives = []
+    cur = db.execute('SELECT adjective FROM Adjectives')
+    for row in cur:
+        adjectives.append(list(row))
+
+    return render_template('index.html',
+    genres=genres,
+    adjectives=adjectives,
+    aesthetics=aesthetics,
+    characters=characters,
+    goals=goals,
+    mechanics=mechanics
+    )
